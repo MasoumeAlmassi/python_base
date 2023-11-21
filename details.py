ae1d24a3164a847f9026441646abf1f03b708dbf
MasoumeAlmassi <m.almassi.m@gmail.com>
2023-11-21 21:47:15 +0000

    details.py completed

---
+++ details.py
@@ -0,0 +1,19 @@
+'''
+START
+1. the user inputs their name
+2. the user inputs their age
+3. the user inputs their house number
+4. the user inputs their street name
+5. print sentence with all user inputs
+END
+'''
+
+name = input("What is your full name?")
+
+age = int(input("How old are you?"))
+
+housenumber = int(input("What is your house number?"))
+
+streetname = input("What is your street name?")
+
+print(f'This is {name}. They are {age} years old and live at house number {housenumber} on {streetname}.')
