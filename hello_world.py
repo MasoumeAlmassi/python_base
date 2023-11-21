cc78eb59c53d9a2582390e4cd80a4704ea44a373
MasoumeAlmassi <m.almassi.m@gmail.com>
2023-11-21 21:27:07 +0000

    hellow world.py completed

---
+++ hello_world.py
@@ -0,0 +1,17 @@
+'''
+START
+1. the user inputs their name
+2. their name is printed
+3. the user inputs their age
+4. their age is printed
+5. print "hello world"
+END
+'''
+
+name = input("What is your name?")
+print(name)
+
+age = int(input("How old are you?"))
+print(age)
+
+print("Hello World!")
