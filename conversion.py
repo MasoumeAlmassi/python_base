6785afe55350f1697e699e32215b1645d56d9dc0
MasoumeAlmassi <m.almassi.m@gmail.com>
2023-11-21 22:32:35 +0000

    conversion.py finished

---
+++ conversion.py
@@ -0,0 +1,52 @@
+'''
+START
+1. Declare variables num1, num2, num3 and string1
+2. Convert variables to integer, float, string and integer, respectively
+3. Print each variable
+END
+'''
+
+#####################
+# Declare variables #
+#####################
+
+num1 = 99.3
+num2 = 23
+num3 = 150
+string1 = "100"
+
+#######################################
+# Checking data types before changing #
+#######################################
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(string1))
+
+#####################
+# Convert variables #
+#####################
+
+num1 = int(num1)
+num2 = float(num2)
+num3 = str(num3)
+string1 = int(string1)
+
+#######################
+# Print all variables #
+#######################
+
+print(num1)
+print(num2)
+print(num3)
+print(string1)
+
+##############################
+# Testing updated data types #
+##############################
+
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print(type(string1))
