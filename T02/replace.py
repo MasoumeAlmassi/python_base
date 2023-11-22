df23628cb2a390dbd0040ddb64309a897cd1f1c6
MasoumeAlmassi <m.almassi.m@gmail.com>
2023-11-22 21:59:09 +0000

    replace.py

--- T02/replace.py
+++ T02/replace.py
@@ -1,1 +1,12 @@
-# PLaceholder
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+print(f'The original sentence is: {sentence}')
+
+sentence = sentence.replace("!"," ")
+print(f'Replacing ! with spaces, the sentence is now: {sentence}')
+
+
+sentence = sentence.upper()
+print(f'Converting the sentence to upper case, the sentence is now: {sentence}')
+
+sentence = sentence[::-1]
+print(f'Printing in reverse by a backward slice function, the sentence is now: {sentence}')
